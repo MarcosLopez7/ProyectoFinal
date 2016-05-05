fd1089277aec957f329aafeb49f8436231021e4d
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-05-05 08:44:31 -0500

    Haciendo un login sin ninguna funcionalidad

--- app/src/main/java/com/marcoslopez7/proyectofinal/MainActivity.java
+++ app/src/main/java/com/marcoslopez7/proyectofinal/MainActivity.java
@@ -2,12 +2,41 @@ package com.marcoslopez7.proyectofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailField;
+    private EditText passwordField;
+    private Button loginButton;
+    private Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailField = (EditText) findViewById(R.id.emailField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        createButton = (Button) findViewById(R.id.createButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login(){
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
     }
 }
