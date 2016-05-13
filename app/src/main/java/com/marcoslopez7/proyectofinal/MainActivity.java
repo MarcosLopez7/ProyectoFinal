cb398cdd0c9fade8d077fce3285da73b4c7aa494
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-05-13 14:35:02 -0500

    Django no quiere, pero lo vamos a forzar

--- app/src/main/java/com/marcoslopez7/proyectofinal/MainActivity.java
+++ app/src/main/java/com/marcoslopez7/proyectofinal/MainActivity.java
@@ -1,13 +1,25 @@
 package com.marcoslopez7.proyectofinal;
 
+import android.app.DownloadManager;
+import android.app.VoiceInteractor;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText passwordField;
     private Button loginButton;
     private Button createButton;
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private OkHttpClient client;
+    private static final String url = "http://159.203.166.99:8000/products/login/";
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +50,36 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void login(){
-        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        OkHttpClient client = new OkHttpClient();
 
+    private void login() {
+        client = new OkHttpClient();
+        RequestBody body = new FormBody.Builder().add("email", emailField.getText().toString()).
+                add("password", passwordField.getText().toString()).build();
+
+        Request request = new Request.Builder().url(url).post(body).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, response.body().string());
+            }
+        });
+
+        /*try {
+
+            Response response = client.newCall(request).execute();
+
+            if(!response.isSuccessful()) throw new IOException("Error en conexion " + response);
+
+            Log.d(TAG, response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
+
 }
