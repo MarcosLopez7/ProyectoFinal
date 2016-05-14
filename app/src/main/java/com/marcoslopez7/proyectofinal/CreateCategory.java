fc74e0869516de7250c9ce67699d85118aedff93
daniel <danielmonr@gmail.com>
2016-05-14 17:13:43 -0500

    Crear categoria

---
+++ app/src/main/java/com/marcoslopez7/proyectofinal/CreateCategory.java
@@ -0,0 +1,72 @@
+package com.marcoslopez7.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class CreateCategory extends AppCompatActivity {
+
+    Button b_submit;
+    EditText et_name, et_descripcion;
+    private static final String url = "http://159.203.166.99:8000/products/createcategoria/", TAG = CreateUserActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_category);
+        init();
+        b_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submit();
+            }
+        });
+
+    }
+
+    private void init(){
+        b_submit = (Button) findViewById(R.id.b_submit);
+        et_name = (EditText) findViewById(R.id.et_name);
+        et_descripcion = (EditText) findViewById(R.id.et_description);
+    }
+
+    private void submit(){
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("nombre", et_name.getText().toString())
+                .addFormDataPart("descripcion", et_descripcion.getText().toString())
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, response.body().string());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/marcoslopez7/proyectofinal/SessionHelper.java
@@ -0,0 +1,15 @@
+package com.marcoslopez7.proyectofinal;
+
+/**
+ * Created by daniel on 14/05/16.
+ */
+public final class SessionHelper {
+
+    private SessionHelper() {
+    admin_user = false;
+    logged_in = false;
+    id_user = -1;}
+    public static int id_user;
+    public static boolean admin_user;
+    public static boolean logged_in;
+}
